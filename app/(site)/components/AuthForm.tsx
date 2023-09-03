783da4a7e810b61f43e77ac48fdb3f446575b240
omarsalah25 <55956352+omarsalah25@users.noreply.github.com>
2023-09-03 14:16:55 +0300

    login/register

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -5,155 +5,182 @@ import {useState,useCallback} from 'react'
 import { useForm ,FieldValues,SubmitHandler} from 'react-hook-form';
 import AuthSocialButton from './AuthSocialButton';
 import {BsGithub,BsGoogle} from 'react-icons/bs'
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
+
 
 type Variant = 'LOGIN'|'REGISTER'
 
-const AuthForm=()=>{
-    const [variant, setVariant] = useState<Variant>("LOGIN")
-    const [isLoading, setIsLoading] = useState(false)
 
-const toggleVariant= useCallback(
-    () => {
-        if(variant=== 'LOGIN')
-        {
-            setVariant('REGISTER')
-        }
-        else{
-            setVariant('LOGIN')
+            const AuthForm=()=>{
+                const [variant, setVariant] = useState<Variant>("LOGIN")
+                const [isLoading, setIsLoading] = useState(false)
 
-        }
-    },[variant]
-)
+            const toggleVariant= useCallback(
+                () => {
+                    if(variant=== 'LOGIN')
+                    {
+                        setVariant('REGISTER')
+                    }
+                    else{
+                        setVariant('LOGIN')
 
-const{
-    register,handleSubmit,formState:{
-        errors
-    }
-} =useForm<FieldValues>({
-    defaultValues:{
-        name:'',
-        email:'',
-        password:''
-    }
-})
+                    }
+                },[variant]
+            )
 
-const onSubmit:SubmitHandler<FieldValues>=(data)=>{
+            const{
+                register,handleSubmit,formState:{
+                    errors
+                }
+            } =useForm<FieldValues>({
+                defaultValues:{
+                    name:'',
+                    email:'',
+                    password:''
+                }
+            })
 
-    setIsLoading(true)
-    if(variant==='REGISTER'){
-        
-    }
-    if(variant==='LOGIN'){
-        
-    }
+            const onSubmit:SubmitHandler<FieldValues>=(data)=>{
 
-}
+                setIsLoading(true)
+                if(variant==='REGISTER'){
+                    axios.post('/api/register',data)
+                    .catch(() => toast.error('Something went wrong!'))
+                    .finally(()=> setIsLoading(false))
+                    
+                }
+                if(variant==='LOGIN'){
+                    signIn('credentials',{
+                        ...data,
+                        redirect:false
+                    })
+                    .then((callback)=>{
+                        if(callback?.error){
+                            toast.error('Invalid credentials')
+                        }
 
-const socialAction =(action:string)=>{
-    setIsLoading(true);
+                        if(callback?.ok&& !callback?.error){
+                            toast.success('Logged in!')
+                        }
+                    })
+                    .finally(()=> setIsLoading(false))
+                    
+                }
+
+            }
+
+            const socialAction =(action:string)=>{
+                setIsLoading(true);
+
+                signIn(action,{redirect:false})
+                .then((callback)=>{
+                    if(callback?.error){
+                        toast.error('Invalid credentials')
+                    }
+
+                    if(callback?.ok&& !callback?.error){
+                        toast.success('Logged in!')
+                    }
+                })
+                .finally(()=> setIsLoading(false))
+            }
+            return(
+         <div className='
+                mt-8
+                sm:mx-auto
+                sm:w-full 
+                sm:max-w-md'>
+
+             <div
+                    className='bg-white
+                    px-4 py-8
+                    shadow
+                    sm:rounded-lg
+                    sm:px-10'>
 
 
-}
-    return(
-        <div className='
-        mt-8
-        sm:mx-auto
-        sm:w-full 
-        sm:max-w-md'>
-
-            <div
-            className='bg-white
-             px-4 py-8
-             shadow
-             sm:rounded-lg
-            sm:px-10'>
-
-
-                <form
-                className='space-y-6'
-                onSubmit={handleSubmit(onSubmit)}>
-{variant === 'REGISTER'&&( <Input
-                 id='name'
-                 label='Name' 
-                 register={register}
-                 errors={errors}
-                 disabled={isLoading}/>
-)}
-                <Input
-                 id='email'
-                 label='Email' 
-                 type='email'
-                 register={register}
-                 errors={errors}
-                 disabled={isLoading}/>
+                        <form
+                        className='space-y-6'
+                        onSubmit={handleSubmit(onSubmit)}>
+                            {variant === 'REGISTER'&&( <Input
+                                id='name'
+                                label='Name' 
+                                register={register}
+                                errors={errors}
+                                disabled={isLoading}/>
+                            )}
+                            <Input
+                                id='email'
+                                label='Email' 
+                                type='email'
+                                register={register}
+                                errors={errors}
+                                disabled={isLoading}/>
 
 
-                 <Input
-                 id='password'
-                 label='Password' 
-                 type='password'
-                 register={register}
-                 errors={errors}
-                 disabled={isLoading}/>
+                            <Input
+                                id='password'
+                                label='Password' 
+                                type='password'
+                                register={register}
+                                errors={errors}
+                                disabled={isLoading}/>
 
-                <div>
-                    <Button disabled={isLoading}
-                    fullWidth
-                    type='submit'
-                    >{variant==='LOGIN' ? 'Sign in' :' Register'}</Button>
+                            <div>
+                                <Button disabled={isLoading}
+                                fullWidth
+                                type='submit'
+                                >{variant==='LOGIN' ? 'Sign in' :' Register'}</Button>
+                            </div>
+
+                        </form>
+                        <div className='mt-6'>
+                            <div className='relative'>
+                                <div className=' absolute inset-0 flex items-center'>
+                                <div className='w-full border-t border-gray-300'/> 
+                                </div>
+                                <div className='relative flex justify-center text-sm'>
+                                    <span className='bg-white px-2 text-gray-500'>
+                                    Or continue with
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div className='mt-6 flex gap-2'>
+                                <AuthSocialButton
+                                    icon={BsGithub}
+                                    onClick={()=>socialAction('github')}/>
+                                <AuthSocialButton
+                                    icon={BsGoogle}
+                                    onClick={()=>socialAction('google')}/>
+                            </div>
+
+                            <div 
+                                className="
+                                    flex 
+                                    gap-2 
+                                    justify-center 
+                                    text-sm 
+                                    mt-6 
+                                    px-2 
+                                    text-gray-500
+                                "
+                                >
+                                <div>
+                                    {variant === 'LOGIN' ? 'New to Crowd Digital?' : 'Already have an account?'} 
+                                </div>
+                                <div 
+                                    onClick={toggleVariant} 
+                                    className="underline cursor-pointer"
+                                >
+                                    {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+                                </div>
+                        </div>
+                    </div>
                 </div>
-
-                </form>
-<div className='mt-6'>
-    <div className='relative'>
-                <div className=' absolute inset-0 flex items-center'>
-
-                <div className='w-full border-t border-gray-300'/> 
-    </div>
-
-        <div className='relative flex justify-center text-sm'>
-            <span className='bg-white px-2 text-gray-500'>
-                Or continue with
-            </span>
-        </div>
-    </div>
-
-    <div className='mt-6 flex gap-2'>
-        <AuthSocialButton
-        icon={BsGithub}
-        onClick={()=>socialAction('github')}/>
-         <AuthSocialButton
-        icon={BsGoogle}
-        onClick={()=>socialAction('google')}/>
-
-    </div>
-
-    <div 
-          className="
-            flex 
-            gap-2 
-            justify-center 
-            text-sm 
-            mt-6 
-            px-2 
-            text-gray-500
-          "
-        >
-          <div>
-            {variant === 'LOGIN' ? 'New to Crowd Digital?' : 'Already have an account?'} 
-          </div>
-          <div 
-            onClick={toggleVariant} 
-            className="underline cursor-pointer"
-          >
-            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
-          </div>
-        </div>
-
-</div>
-
             </div>
-        </div>
-    );
+            );
 }
 export default AuthForm
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
-import "./globals.css"
+import AuthContext from "./(site)/context/AuthContext"
+import ToasterContext from "./(site)/context/ToasterContext"
 import type {Metadata} from "next"
 import {Inter} from "next/font/google"
+import "./globals.css"
+
 
 const inter = Inter({subsets: ["latin"]})
 
-export const metadata: Metadata = {
+export const metadata: Metadata =
+{
 	title: "Crowd Teams",
 	description: "Crowd Teams",
 }
@@ -12,7 +16,12 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<AuthContext>
+					<ToasterContext/>
+					{children}
+				</AuthContext>
+			</body>
 		</html>
 	)
 }
